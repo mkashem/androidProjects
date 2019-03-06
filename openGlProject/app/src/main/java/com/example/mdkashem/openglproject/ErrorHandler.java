c3a86cc198462fcb441d371772786dfe82b497b9
mkashem@wvup.edu <mkashem@wvup.edu>
2019-03-06 11:00:07 -0500

    update

--- openGlProject/app/src/main/java/com/example/mdkashem/openglproject/ErrorHandler.java
+++
@@ -1,9 +0,0 @@
-package com.example.mdkashem.openglproject;
-
-interface ErrorHandler {
-	enum ErrorType {
-		BUFFER_CREATION_ERROR
-	}
-	
-	void handleError(ErrorType errorType, String cause);
-}
--- openGlProjectTwo/app/src/main/java/com/example/mdkashem/openglprojecttwo/ErrorHandler.java
+++
@@ -1,9 +0,0 @@
-package com.example.mdkashem.openglprojecttwo;
-
-interface ErrorHandler {
-	enum ErrorType {
-		BUFFER_CREATION_ERROR
-	}
-	
-	void handleError(ErrorType errorType, String cause);
-}
